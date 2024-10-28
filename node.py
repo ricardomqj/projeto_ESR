f1faae02fdd6ba30e7a823dd506bc2f6d07bf07f
Rui Pinto <94129877+RuiPintoUM@users.noreply.github.com>
2024-10-28 17:28:08 +0000

    nodes a ligar só com uma porta

--- node.py
+++ node.py
@@ -4,32 +4,42 @@ import threading
 
 connectionsIP = []
 
-def handle_connections(node_ip, forward_ip, porta):
+def handle_connections():
+    global connectionsIP
+
     # Cria um socket UDP para este nó
     connection_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Bind do socket a um IP e porta locais (o nó vai ouvir nesta porta)
-    local_address = ('0.0.0.0', porta) 
+    local_address = ('0.0.0.0', 9090) 
     connection_socket.bind(local_address)
 
-    print(f"Esperando mensagens de {node_ip} para reencaminhar para {forward_ip}...")
-
-    assigned_port = connection_socket.getsockname()[1]
-    print(f"Socket vinculado a porta: {assigned_port}")
-
     while True:
-        # Receber a mensagem do nó atual
+
         data, sender_address = connection_socket.recvfrom(1024)
         mensagem = data.decode()
 
         print(f"Mensagem recebida de {sender_address}: {mensagem}")
+        
+        print(f"olha a lista {connectionsIP}")
+        for val in connectionsIP:
+            print(val)
+            if val != sender_address:
+                forward_ip = val
+                print(forward_ip)
+                break
 
-        # Enviar a mensagem para o nó com o qual está conectado
-        connection_socket.sendto(mensagem.encode(), (forward_ip, porta))
-        print(f"Mensagem reencaminhada para {forward_ip}")
+        if forward_ip:
+            # Enviar a mensagem para o nó com o qual está conectado
+            connection_socket.sendto(mensagem.encode(), (forward_ip, 9090))
+            print(f"Mensagem reencaminhada para {forward_ip}")
+        else:
+            print("Nenhum IP para reencaminhar a mensagem.")
 
 
 def cliente():
+    global connectionsIP
+    
     if len(sys.argv) != 2:
         print("Uso: python3 client.py <IP_SERVIDOR>")
         sys.exit(1)
@@ -52,11 +62,9 @@ def cliente():
         mensagem = data.decode()
         connectionsIP = mensagem.split(', ')
 
+        print(connectionsIP)
         
-        mesage_thread = threading.Thread(target =handle_connections, args=(connectionsIP[0], connectionsIP[1], 9091))
-        mesage_thread.start()
-
-        mesage_thread = threading.Thread(target =handle_connections, args=(connectionsIP[1], connectionsIP[0], 9090))
+        mesage_thread = threading.Thread(target =handle_connections, args=())
         mesage_thread.start()
 
         while True:
@@ -69,3 +77,4 @@ def cliente():
 
 if __name__ == "__main__":
     cliente()
+
