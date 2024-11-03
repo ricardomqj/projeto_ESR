1454926230ca4b63e9cf96b46e723944a61d83b9
ricardomqj <ricardojesus189@gmail.com>
2024-11-03 05:27:21 +0000

    Streaming a dar

---
+++ ServerWorker.py
@@ -0,0 +1,175 @@
+from random import randint
+import sys, traceback, threading, socket
+
+from VideoStream import VideoStream
+from RtpPacket import RtpPacket
+
+class ServerWorker:
+	SETUP = 'SETUP'
+	PLAY = 'PLAY'
+	PAUSE = 'PAUSE'
+	TEARDOWN = 'TEARDOWN'
+	
+	INIT = 0
+	READY = 1
+	PLAYING = 2
+	state = INIT
+
+	OK_200 = 0
+	FILE_NOT_FOUND_404 = 1
+
+	CON_ERR_500 = 2
+	
+	clientInfo = {}
+	
+	def __init__(self, clientInfo):
+		self.clientInfo = clientInfo
+		
+	def run(self):
+		threading.Thread(target=self.recvRtspRequest).start()
+	
+	def recvRtspRequest(self):
+		"""Receive RTSP request from the client."""
+		connSocket = self.clientInfo['rtspSocket'][0]
+		while True:            
+			data = connSocket.recv(256)
+			if data:
+				print("Data received:\n" + data.decode("utf-8"))
+				self.processRtspRequest(data.decode("utf-8"))
+	
+	def processRtspRequest(self, data):
+		"""Process RTSP request sent from the client."""
+		# Get the request type
+		request = data.split('\n')
+		line1 = request[0].split(' ')
+		requestType = line1[0]
+		
+		# Get the media file name
+		filename = line1[1]
+		
+		# Get the RTSP sequence number 
+		seq = request[1].split(' ')
+		
+		# Process SETUP request
+		if requestType == self.SETUP:
+			if self.state == self.INIT:
+				# Update state
+				print("processing SETUP\n")
+				
+				try:
+					self.clientInfo['videoStream'] = VideoStream(filename)
+					self.state = self.READY
+				except IOError:
+					self.replyRtsp(self.FILE_NOT_FOUND_404, seq[1])
+				
+				# Generate a randomized RTSP session ID
+				self.clientInfo['session'] = randint(100000, 999999)
+				
+				# Send RTSP reply
+				self.replyRtsp(self.OK_200, seq[1])
+				
+				# Get the RTP/UDP port from the last line
+				self.clientInfo['rtpPort'] = request[2].split(' ')[3]
+		
+		# Process PLAY request 		
+		elif requestType == self.PLAY:
+			if self.state == self.READY:
+				print("processing PLAY\n")
+				self.state = self.PLAYING
+				
+				# fechar rtspSocket para ver se não dá problemas de address already in use
+				#self.clientInfo['rtspSocket'][0].close() -> dá erro de Bad file descriptor esta malandrice
+				# Create a new socket for RTP/UDP
+				self.clientInfo["rtpSocket"] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+				
+				self.replyRtsp(self.OK_200, seq[1])
+				
+				# Create a new thread and start sending RTP packets
+				self.clientInfo['event'] = threading.Event()
+				print("[processingRtspRequest] starting a thread to send Rtp")
+				self.clientInfo['worker']= threading.Thread(target=self.sendRtp) 
+				self.clientInfo['worker'].start()
+		"""
+		# Process PAUSE request
+		elif requestType == self.PAUSE:
+			if self.state == self.PLAYING:
+				print("processing PAUSE\n")
+				self.state = self.READY
+				
+				self.clientInfo['event'].set()
+			
+				self.replyRtsp(self.OK_200, seq[1])
+		
+		# Process TEARDOWN request
+		elif requestType == self.TEARDOWN:
+			print("processing TEARDOWN\n")
+
+			self.clientInfo['event'].set()
+			
+			self.replyRtsp(self.OK_200, seq[1])
+			
+			# Close the RTP socket
+			self.clientInfo['rtpSocket'].close()
+		"""	
+	def sendRtp(self):
+		"""Send RTP packets over UDP."""
+		print("[sendRtp]Inside sendRtp function on ServerWorker.py")
+		while True:
+			self.clientInfo['event'].wait(0.05) 
+			
+			# Stop sending if request is PAUSE or TEARDOWN
+			if self.clientInfo['event'].isSet(): 
+				break 
+				
+			data = self.clientInfo['videoStream'].nextFrame()
+			if data: 
+				frameNumber = self.clientInfo['videoStream'].frameNbr()
+				try:
+					address = self.clientInfo['rtspSocket'][1][0]
+					port = int(self.clientInfo['rtpPort'])
+					print(f"[sendRtp]Antes de clientInfo['rtpSocket'].sendto(self.makeRTP) to the the follwing addr -> {(address, port)}")
+					self.clientInfo['rtpSocket'].sendto(self.makeRtp(data, frameNumber),(address,port))
+					print(f"[sendRtp] sending the client the following data:\nframenumber->{frameNumber}\n{data}\n________________\n")
+					print(f"sended to the follwing address -> {(address, port)}")
+
+				except:
+					print("Connection Error")
+					#print('-'*60)
+					#traceback.print_exc(file=sys.stdout)
+					#print('-'*60)
+
+
+	def makeRtp(self, payload, frameNbr):
+		"""RTP-packetize the video data."""
+		print("[makeRtp]Making rtp")
+		version = 2
+		padding = 0
+		extension = 0
+		cc = 0
+		marker = 0
+		pt = 26 # MJPEG type
+		seqnum = frameNbr
+		ssrc = 0 
+		
+		rtpPacket = RtpPacket()
+		
+		rtpPacket.encode(version, padding, extension, cc, seqnum, marker, pt, ssrc, payload)
+		
+		print("[makeRTP] returning RTP packet")
+		return rtpPacket.getPacket()
+		
+	def replyRtsp(self, code, seq):
+		"""Send RTSP reply to the client."""
+		print("[replyRtsp] inside replyRtsp function")
+		if code == self.OK_200:
+			#print("200 OK")
+			reply = 'RTSP/1.0 200 OK\nCSeq: ' + seq + '\nSession: ' + str(self.clientInfo['session'])
+			connSocket = self.clientInfo['rtspSocket'][0]
+			print(f"[replyRtsp] sending reply: {reply}")
+			connSocket.send(reply.encode())
+		
+		# Error messages
+		elif code == self.FILE_NOT_FOUND_404:
+			print("404 NOT FOUND")
+		elif code == self.CON_ERR_500:
+			print("500 CONNECTION ERROR")
--- server.py
+++ server.py
@@ -1,60 +1,25 @@
-import socket
-import threading
+import sys, socket
 
-# Dicionário com os IPs de cada nó e os IPs para os quais o nó deve se conectar
-nodes_conections = {
-    '10.0.0.1': ['10.0.0.10', '10.0.1.2'],
-    '10.0.1.2': ['10.0.0.1', '10.0.10.1'],
-    '10.0.10.1': ['10.0.1.2', '10.0.6.2'],
-    '10.0.6.2': ['10.0.10.1', '10.0.12.20']
-}
+from ServerWorker import ServerWorker
 
-# Função que lida com o cliente em uma thread separada
-def handle_client(client_address, server_socket, mensagem):
-    try:
-        if mensagem == "connecting":
-            ip_cliente = client_address[0]
-            print(f"IP recebido de {client_address}: {ip_cliente}")
+class Server:	
+	
+	def main(self):
+		try:
+			SERVER_PORT = int(sys.argv[1])
+		except:
+			print("[Usage: Server.py Server_port]\n")
+		rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		rtspSocket.bind(('', SERVER_PORT))
+		rtspSocket.listen(5)        
 
-            # Verifica se o IP recebido está no dicionário
-            if ip_cliente in nodes_conections:
-                # Obtém os IPs aos quais o nó deve se conectar
-                ips_a_ligar = nodes_conections[ip_cliente]
-
-                # Converte a lista de IPs para uma string separada por vírgulas
-                mensagem = ', '.join(ips_a_ligar)
-                print(f"Mensagem enviada para {client_address}: {mensagem}")
-
-                # Envia a string de volta ao cliente
-                server_socket.sendto(mensagem.encode(), client_address)
-            else:
-                print(f"IP {ip_cliente} não encontrado nas conexões.")
-        else:
-            print(mensagem)
-    except Exception as e:
-        print(f"Erro ao processar o cliente {client_address}: {e}")
-
-# Função do servidor que aceita conexões
-def servidor():
-    # Criação do socket UDP
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # Definir o endereço e a porta do servidor
-    server_address = ('0.0.0.0', 9090)  # O servidor vai aceitar conexões de qualquer IP
-    server_socket.bind(server_address)
-
-    print(f"Servidor UDP ouvindo em {server_address}")
-
-    while True:
-        # Recebe a mensagem do cliente e o endereço de onde veio
-        data, client_address = server_socket.recvfrom(1024)
-
-        mensagem = data.decode()
-        print(mensagem)
-
-        # Cria uma thread para lidar com o cliente
-        client_thread = threading.Thread(target=handle_client, args=(client_address, server_socket, mensagem))
-        client_thread.start()
+		# Receive client info (address,port) through RTSP/TCP session
+		while True:
+			clientInfo = {}
+			clientInfo['rtspSocket'] = rtspSocket.accept()
+			ServerWorker(clientInfo).run()		
 
 if __name__ == "__main__":
-    servidor()
+	(Server()).main()
+
+
